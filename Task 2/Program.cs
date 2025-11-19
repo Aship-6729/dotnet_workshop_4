f4dd563ce9f062fcf41b931fc8cb9077c23b4675
Aship-6729 <aalamaship98@gmail.com>
2025-11-19 12:06:32 +0545

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Calculator c= new Calculator();
+            
+            c.PrintWelcome();
+            Console.WriteLine($"\nAddition: {c.Add(5, 10)}\nMultiplication: {c.Multiply(5,7)}\n");
+        }
+    }
+}
