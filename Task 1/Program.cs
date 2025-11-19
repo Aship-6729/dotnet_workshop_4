3571d5445e0765a78d457ed358a3f9b4245cb1df
Aship-6729 <aalamaship98@gmail.com>
2025-11-19 11:47:03 +0545

    Task1

---
+++ Task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            Student s1 = new Student();
+
+            s1.student_Id = 101;
+            s1.student_Name = "Alice";
+            s1.student_Age = 20;
+
+            Student s2 = new(); // another way or creating object.
+            s2.student_Id = 102;
+            s2.student_Name = "Bob";
+            s2.student_Age = 22;
+
+            Console.WriteLine($"Student 1 Details: \n {s1.student_Id}\n{s1.student_Name}\n{s1.student_Age}");
+            Console.WriteLine($"\nStudent 1 Details: \n {s2.student_Id}\n{s2.student_Name}\n{s2.student_Age}");
+            Console.WriteLine($"\nPrinting Static Field{Student.student_College}");
+        }
+    }
+}
---
+++ Task 1/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class Student
+    {
+        // defaultlty the access modifier is private..
+        public int student_Id;
+        public string student_Name;
+        public int student_Age;
+
+        public static string student_College;
+
+    }
+}
