8b0acbd3b0537d52dcb048c18e5ee5c75120ec0b
Aship-6729 <aalamaship98@gmail.com>
2025-11-20 10:13:49 +0545

    Task 4

---
+++ Task 4/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_4
+{
+    internal class Player
+    {
+        public string playerName;
+        public int level;
+        public int health;
+
+
+        public Player()
+        {
+            Console.WriteLine("Default constructor has been called");
+        }
+
+        public Player(string name, int lvl, int hp)
+        {
+            playerName = name;
+            level = lvl;
+            health = hp;
+            Console.WriteLine("Parameterized constructor has been called");
+        }
+    }
+}
---
+++ Task 4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            Player p1 = new Player();// obj using default constructor.
+            Player p2 = new Player("Hero", 10, 100); // obj using parameterized constructor.
+
+            Console.WriteLine($"Player 1: Name={p1.playerName}, Level={p1.level}, Health={p1.health}");
+            Console.WriteLine($"Player 2: Name={p2.playerName}, Level={p2.level}, Health={p2.health}");
+        }
+    }
+}
