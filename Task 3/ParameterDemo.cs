b11a8cd2ee3ce7204c6ecb90031e63c724261819
Aship-6729 <aalamaship98@gmail.com>
2025-11-20 10:13:19 +0545

    Task 3

---
+++ Task 3/ParameterDemo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public class ParameterDemo
+    {
+        public void Increase(ref int number)
+        {
+            number += 10;
+        }
+
+        public void GetFullName(out string fullName)
+        {
+            fullName = "John Doe";
+        }
+
+        public int SumAll(params int[] numbers)
+        {
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Task 3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            ParameterDemo pd = new ParameterDemo();
+            int number = 5;
+
+            Console.WriteLine($"Original number: {number}");
+            pd.Increase(ref number);
+            Console.WriteLine($"After Increase method: {number}");
+            Console.WriteLine();
+
+            pd.GetFullName(out string fullName);
+            Console.WriteLine($"Full Name: {fullName}");
+            Console.WriteLine();
+
+            int sum1 = pd.SumAll(1, 2, 3, 4, 5);
+            Console.WriteLine($"Sum of 1,2,3,4,5: {sum1}");
+        }
+    }
+}
